74a062e7bd1c6acf754306ed2d4de34b53b38259
herrkaefer <gloolar@gmail.com>
2024-08-15 14:20:18 -0500

    v1.2.0

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
-// Copyright (c) 2018 Yang LIU (gloolar at gmail.com)
+// Copyright (c) 2018-2014 Yang LIU (gloolar at gmail.com)
 
 'use strict';
-
-// URLs to be closed
-// var urlRules = /chrome:\/\/|www.google.com\/search?|stackoverflow.com\/questions|www.bing.com\/search?|www.baidu.com\/s?|.taobao.com|.tmall.com/;
 
 var defaultUrlRules = [
   "chrome://",
@@ -11,29 +8,24 @@ var defaultUrlRules = [
   "www.bing.com/search?",
   "www.baidu.com/s?",
   "stackoverflow.com/questions"
-]
+];
 
 function closeTabs() {
   chrome.storage.sync.get(['urlRules'], function(data) {
     var urlRules = data.urlRules;
     if (urlRules.length > 0) {
       chrome.tabs.query({
-          active: false,
-          pinned: false,
-          currentWindow: true
-        }, function(tabs) {
-          var tabsToClose = [];
-          for (var i = 0; i < tabs.length; i++) {
-            for (var j = 0; j < urlRules.length; j++) {
-              if (tabs[i].url.includes(urlRules[j])) {
-                tabsToClose.push(tabs[i].id);
-                break;
-              }
-            }
-          }
-          console.log(tabsToClose.length + ' tabs to close: ' + tabsToClose);
-          chrome.tabs.remove(tabsToClose);
-        });
+        active: false,
+        pinned: false,
+        currentWindow: true
+      }, function(tabs) {
+        var tabsToClose = tabs.filter(tab =>
+          urlRules.some(rule => tab.url.includes(rule))
+        ).map(tab => tab.id);
+
+        console.log(tabsToClose.length + ' tabs to close: ' + tabsToClose);
+        chrome.tabs.remove(tabsToClose);
+      });
     }
   });
 }
@@ -44,4 +36,4 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-chrome.browserAction.onClicked.addListener(closeTabs);
+chrome.action.onClicked.addListener(closeTabs);
--- options.js
+++ options.js
@@ -1,26 +1,22 @@
-
 // Saves options to chrome.storage
 function save_options() {
-  var urlRules = String(document.getElementById('urlRules').value)
-                 .trim()
-                 .split('\n')
-                 .map(x=>x.trim())
-                 .filter(x=>x);
-  chrome.storage.sync.set({urlRules: urlRules}, function() {
+  const urlRules = document.getElementById('urlRules').value
+                   .trim()
+                   .split('\n')
+                   .map(x => x.trim())
+                   .filter(Boolean);
+  chrome.storage.sync.set({urlRules}, function() {
     restore_options();
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
+    const status = document.getElementById('status');
     status.textContent = 'Options saved.';
-    setTimeout(function() {
+    setTimeout(() => {
       status.textContent = '';
     }, 750);
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores the options stored in chrome.storage
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get('urlRules', function(data) {
     document.getElementById('urlRules').value = data.urlRules.join("\n");
   });
